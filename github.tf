5cfb3a42ba4c402c5b0f21300b86180460a290f0
Aaron Trout <aaron.trout@limejump.com>
2020-11-24 10:05:15 +0000

    Properly support user provided private key
    This change will allow the public key to be added to the GitHub
    repo even if the private key is provided by the user and not
    generated by the module.

--- github.tf
+++ github.tf
@@ -7,19 +7,24 @@ resource "tls_private_key" "flux" {
   rsa_bits  = 2048
 }
 
+data "tls_public_key" "user_provided" {
+  count           = var.private_key_pem == null ? 0 : 1
+  private_key_pem = var.private_key_pem
+}
+
 data "github_repository" "flux-repo" {
   count     = var.github_install_deploy_key == true ? 1 : 0
   full_name = "${var.github_repository_owner}/${var.github_repository_name}"
 }
 
 resource "github_repository_deploy_key" "flux" {
-  count      = var.private_key_pem == null ? (var.github_install_deploy_key == true ? 1 : 0) : (0)
+  count      = var.github_install_deploy_key == true ? 1 : 0
   title      = var.github_deploy_key_title
   repository = data.github_repository.flux-repo[0].name
   read_only  = false
-  key        = tls_private_key.flux[0].public_key_openssh
+  key        = length(tls_private_key.flux) > 0 ? tls_private_key.flux[0].public_key_openssh : data.tls_public_key.user_provided[0].public_key_openssh
 }
 
 output "public_key_openssh" {
-  value = tls_private_key.flux[0].public_key_openssh
+  value = length(tls_private_key.flux) > 0 ? tls_private_key.flux[0].public_key_openssh : data.tls_public_key.user_provided[0].public_key_openssh
 }
